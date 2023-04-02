5d338b680043ee7c0cccf8b86b830e59de383786
PankajAgrawal12 <104184475+PankajAgrawal12@users.noreply.github.com>
2023-04-02 15:02:42 +0530

    Time: 23 ms (50.61%), Space: 14.4 MB (36.52%) - LeetHub

--- 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
+++ 0188-best-time-to-buy-and-sell-stock-iv/0188-best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,28 @@
 class Solution {
 public:
-    int fun(int index,int buy,int cap,vector<int>&prices,vector<vector<vector<int>>> &dp)
-    {
-        if(index==prices.size())
-            return 0;
-        if(cap==0)
-            return 0;
-        if(dp[index][buy][cap]!=-1)
-            return dp[index][buy][cap];
-        int profit=0;
-        if(buy)
-        {
-            profit=max(-prices[index]+fun(index+1,0,cap,prices,dp),fun(index+1,1,cap,prices,dp));
-        }
-        else
-        {
-             profit=max(prices[index]+fun(index+1,1,cap-1,prices,dp),fun(index+1,0,cap,prices,dp));
-        }
-        
-        return dp[index][buy][cap]=profit;
-    }
     int maxProfit(int k, vector<int>& prices) {
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int>(k+1,-1)));
-        return fun(0,1,k,prices,dp);
+        vector<vector<vector<int>>> dp(n+1,vector<vector<int>> (2,vector<int>(k+1,0)));
+        for(int index=n-1;index>=0;index--)
+        {
+            for(int buy=0;buy<=1;buy++)
+            {
+                for(int cap=1;cap<=k;cap++)
+                {
+                    int profit=0;
+                    if(buy)
+                    {
+                        profit=max(-prices[index]+dp[index+1][0][cap],dp[index+1][1][cap]);
+                    }
+                    else
+                    {
+                         profit=max(prices[index]+dp[index+1][1][cap-1],dp[index+1][0][cap]);
+                    }
+
+                    dp[index][buy][cap]=profit;
+                }
+            }
+        }
+        return dp[0][1][k];
     }
 };
